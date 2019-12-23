f2ef35c15ee89482c2c5553c9044c40a01e7bc7c
Rosano Coutinho <pub@xeari.com>
2019-12-23 18:18:11 -0500

    Purge post install

--- package-postinstall.js
+++
@@ -1,7 +0,0 @@
-require('./node_modules/OLSKApp/modules/OLSKAssets/main.js').OLSKAssetsCopyAssetsFromTo([
-		'OLSKLanguageSwitcher',
-		'OLSKUIAssets',
-
-		// pass tests
-		'OLSKReloadButton',
-	], require('path').join(__dirname, 'node_modules'), require('path').join(__dirname, 'os-app/_shared/__external'));
