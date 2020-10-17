eae03fa3fe261363570820d7fbe109a904c27f4a
Rosano Coutinho <pub@xeari.com>
2020-10-17 10:31:15 -0300

    Purge excess function identifiers

--- rollup-start.js
+++ rollup-start.js
@@ -3,14 +3,14 @@ import RollupStart from './main.svelte';
 const OLSKAppToolbar = new RollupStart({
 	target: document.body,
 	props: Object.assign({
-		OLSKAppToolbarDispatchLauncher: (function _OLSKAppToolbarDispatchLauncher () {
+		OLSKAppToolbarDispatchLauncher: (function  () {
 			window.TestOLSKAppToolbarDispatchLauncher.innerHTML = parseInt(window.TestOLSKAppToolbarDispatchLauncher.innerHTML) + 1;
 		}),
-		OLSKAppToolbarDispatchStorage: (function _OLSKAppToolbarDispatchStorage () {
+		OLSKAppToolbarDispatchStorage: (function  () {
 			window.TestOLSKAppToolbarDispatchStorage.innerHTML = parseInt(window.TestOLSKAppToolbarDispatchStorage.innerHTML) + 1;
 		}),
-		_OLSKAppToolbarDispatchExport: (function __OLSKAppToolbarDispatchExport () {}),
-		_OLSKAppToolbarDispatchImport: (function __OLSKAppToolbarDispatchImport () {}),
+		_OLSKAppToolbarDispatchExport: (function  () {}),
+		_OLSKAppToolbarDispatchImport: (function  () {}),
 	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()).map(function (e, index, coll) {
 		if (['OLSKAppToolbarDispatchLauncher'].includes(e[0])) {
 			e[1] = JSON.parse(e[1]);
